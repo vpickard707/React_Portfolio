c6875517a90430c590df6d3e00e54e6352d08ba2
vpickard707 <71297613+vpickard707@users.noreply.github.com>
2021-03-17 22:28:50 -0700

    updated link and cv

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -14,7 +14,7 @@ export default class Introduction extends Component {
                       <div className="slider-text-inner js-fullheight">
                         <div className="desc">
                           <h1>Hi! <br />I'm Violet</h1>
-                          <p><a className="btn btn-primary btn-learn" href="" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
+                          <p><a className="btn btn-primary btn-learn" href="https://drive.google.com/file/d/1biEdY9IY55e8Wa7wQJbN7_WoZmT7MsEe/view?usp=sharing" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
                         </div>
                       </div>
                     </div>
