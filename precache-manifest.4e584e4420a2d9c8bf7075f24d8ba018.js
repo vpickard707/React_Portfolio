5dd33bd8c19340121d2fa79fa4261470b7bfbe25
vpickard707 <71297613+vpickard707@users.noreply.github.com>
2021-03-17 18:16:16 -0700

    Updates

---
+++ precache-manifest.4e584e4420a2d9c8bf7075f24d8ba018.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f642097a03f01ccafb8d",
+    "url": "/React_Portfolio/static/css/main.b1da54b1.chunk.css"
+  },
+  {
+    "revision": "f642097a03f01ccafb8d",
+    "url": "/React_Portfolio/static/js/main.f642097a.chunk.js"
+  },
+  {
+    "revision": "b6aa8232b4d54c3ee476",
+    "url": "/React_Portfolio/static/js/runtime~main.b6aa8232.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/React_Portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "3893137cb2d4fdcf66c80d2eb0eca0b3",
+    "url": "/React_Portfolio/index.html"
+  }
+];
